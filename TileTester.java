2351e21cde2c08a9b532049bc53c42cd74a7b29c
Jasmine <jasmine.cole@crpusd.org>
2026-03-05 09:24:11 -0800

    almost done with randomizer, need to shrink original tileValues after taking from it

--- TileTester.java
+++ TileTester.java
@@ -15,7 +15,26 @@ public class TileTester
   {
     
     /* your code here */
-  
+    Tile[][] gameboard = new Tile[3][4];
+    int end = tileValues.length;
+
+    for (Tile[] eRow : gameboard)
+    {
+      for (Tile currentTile : eRow)
+      {
+          int random = (int)(Math.random()*end);
+          currentTile = new Tile(tileValues[random]);
+          end--;
+      }
+    }
+    
+    for (int row = 0; row < gameboard.length; row++)
+    {
+      for (int col = 0; col < gameboard[0].length; col++)
+      {
+        System.out.println(gameboard[row][col]);
+      }
+    }
     
   }
 }
